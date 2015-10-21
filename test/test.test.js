5f8a910e451c009287f7e467dc5ec67c2d219aab
Jaroslav.Popov <jaroslav.popov@takeaway.com>
2015-10-21 17:34:45 +0200

    fix

--- test/test.test.js
+++ test/test.test.js
@@ -1,11 +1,16 @@
 var assert = require('assert'),
     myFunc = require('../index.js');
-describe.only('only path given, absolute path', function () {
-    it('should bubble up every folder and merge corresponding config', function (done) {
-//    var absPath = '/home/jaroslav/repos/magga-merge/fixtures/page/domain/someFolder/menu/test/test.conf';
-        //TODO: Somehow resolving absolute path doesn't work. Fix it
+
+describe('abs path bug', function () {
+    it('should check absolute path', function () {
         var absPath = myFunc('/test/somepath/somefile');
-        //    absPath = path.resolve('/test/fixtures/page/domain/someFolder/menu/test/test.conf');
         assert.equal(absPath,'/home/chilicoder/repos/bug-with-path/test/somepath/somefile');
+    });
+});
+
+describe('abs path normally resolves', function () {
+    it('should check absolute path', function () {
+        var absPath = myFunc('');
+        assert.equal(absPath,process.cwd());
     });
 });
